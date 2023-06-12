9e1d185a3c27a150972c10fadd154cb3a2ffa588
James Bolton <james.bolton@skybettingandgaming.com>
2023-06-12 23:32:33 +0100

    feat: convert script to typeScript

---
+++ packages/zipper/lib/zipper.ts
@@ -0,0 +1,48 @@
+import * as fs from "fs";
+import * as path from "path";
+import * as archiver from "archiver";
+import { program } from "commander";
+
+program
+  .arguments("<files...>")
+  .option("-o, --output <output>", "Output zip file path")
+  .parse(process.argv);
+
+function zipFiles(files: string[], outputFilePath: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const output = fs.createWriteStream(outputFilePath);
+    const archive = archiver("zip", {
+      zlib: { level: 9 }, // Compression level (0-9)
+    });
+
+    output.on("close", () => {
+      console.log("Successfully zipped the files.");
+      resolve();
+    });
+
+    archive.on("error", (err) => {
+      console.error("Failed to zip the files:", err);
+      reject(err);
+    });
+
+    archive.pipe(output);
+
+    for (const filePath of files) {
+      const file = path.basename(filePath);
+      archive.append(fs.createReadStream(filePath), { name: file });
+    }
+
+    archive.finalize();
+  });
+}
+
+const filesToZip = program.args as string[];
+const outputFilePath = program.opts().output || "output.zip";
+
+if (!filesToZip.length || !outputFilePath) {
+  program.help();
+} else {
+  zipFiles(filesToZip, outputFilePath).catch((err) => {
+    console.error("An error occurred while zipping the files:", err);
+  });
+}
